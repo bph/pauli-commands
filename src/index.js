a66b91524bb353a56b7f467b0c6806de3fe92421
Birgit Pauli-Haack <birgit.pauli@gmail.com>
2023-11-17 16:07:49 +0100

    tightened code

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import { __ }       from '@wordpress/i18n';
 import { settings, comment, button } from '@wordpress/icons';
 
 dispatch( store ).registerCommand( {
-	name:  'pauli/gutenberg-experiments',
+	name:  'pauli-commands/gutenberg-experiments',
 	label: __( 'Gutenberg Experiments', 'pauli' ),
 	icon:  settings,
 	context: 'site-editor',
 	callback: ( { close } ) => {
 		document.location.href = 'admin.php?page=gutenberg-experiments';
+		close();
+	}
+} );
+
+
+dispatch( store ).registerCommand( {
+	name:  'pauli-commands/discussion-panel',
+	label: __( 'Toggle Discussion Panel', 'pauli' ),
+	icon:  comment,
+	callback: ( { close } ) => {
+		dispatch( 'core/edit-post' ).toggleEditorPanelEnabled(
+			'discussion-panel'
+		);
 		close();
 	}
 } );
